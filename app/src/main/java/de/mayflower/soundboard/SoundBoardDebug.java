4bf936dca1224cd3d70774d56b67f2dba4bb37d8
Christopher Stock <email@christopherstock.de>
2016-12-02 14:18:04 +0100

    Refactored base project classes.

--- app/src/main/java/de/mayflower/soundboard/SoundBoardDebug.java
+++ app/src/main/java/de/mayflower/soundboard/SoundBoardDebug.java
@@ -2,7 +2,6 @@
     package de.mayflower.soundboard;
 
     import  android.util.Log;
-
     import  de.mayflower.lib.LibDebug;
 
     /*****************************************************************************
@@ -25,9 +24,6 @@
 
         /** The stacktrace-String to send via email. */
         public      static  final   boolean             DEBUG_MODE                  = SoundBoardSettings.Debug.DEBUG_MODE;
-
-        /** The stacktrace-String to send via email. */
-        public      static          String              stackTraceString            = "";
 
         /** The debug flag for this debug group. */
         private                     boolean             debug                       = false;
@@ -68,43 +64,6 @@
         }
 
         /*****************************************************************************
-        *   Shows the stack-trace of the specified throwable in a scrollable dialog.
-        *
-        *   @param  t   The throwable to display in the throwable dialog.
-        *****************************************************************************/
-        private static final void DEBUG_THROWABLE( Throwable t )
-        {
-            DEBUG_THROWABLE( t, null );
-        }
-
-        /*****************************************************************************
-        *   Shows the stack-trace of the specified throwable in a scrollable dialog.
-        *
-        *   @param  t               The throwable to display in the throwable dialog.
-        *   @param  extraMessage    The extra string to display at the end of the
-        *                           stack-trace. This can be any additional information
-        *                           that helps to trace this error, e.g. a http-response-body.
-        *****************************************************************************/
-        public static final void DEBUG_THROWABLE( Throwable t, String extraMessage )
-        {
-            //debug out the throwable
-            DEBUG_OUT_THROWABLE( t );
-
-            //pack stackTraceString
-            stackTraceString =
-            (
-                    "["
-                +   t.getMessage()
-                +   "]["
-                +   t
-                +   "]["
-                +   Log.getStackTraceString( t )
-                +   "]"
-                +   ( extraMessage == null ? "" : "\n\nExtra Message:\n\n" + extraMessage )
-            );
-        }
-
-        /*****************************************************************************
         *   Delegates a message and a output-tag to the system's log system
         *   with a severity of 'info'.
         *
@@ -114,7 +73,7 @@
         *****************************************************************************/
         private static final void DEBUG_OUT( String tag, Object msg )
         {
-            if (DEBUG_MODE)
+            if ( DEBUG_MODE )
             {
                 Log.i( tag, String.valueOf( msg ) );
             }
@@ -129,7 +88,7 @@
         *****************************************************************************/
         private static final void DEBUG_ERR( String tag, Object msg )
         {
-            if (DEBUG_MODE)
+            if ( DEBUG_MODE )
             {
                 Log.e( tag, String.valueOf( msg ) );
             }
@@ -140,7 +99,7 @@
         *
         *   @param  t   The Throwable to print the stack-trace for.
         *****************************************************************************/
-        public static final void DEBUG_OUT_THROWABLE( Throwable t )
+        public static final void DEBUG_THROWABLE(Throwable t )
         {
             if ( DEBUG_MODE )
             {
